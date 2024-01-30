d8580e22b7c130075761c9da3d7387e527e940a8
Pabmoy25 <pabmoy1725@gmail.com>
2024-01-30 16:37:00 -0300

    agregue logica a FormularioTareas

--- src/components/FormularioTareas.jsx
+++ src/components/FormularioTareas.jsx
@@ -1,8 +1,12 @@
 import Form from "react-bootstrap/Form";
 import { Button } from "react-bootstrap";
 import ListaTareas from "./ListaTareas";
+import { useState } from "react";
 
 const FormularioTareas = () => {
+  const [tarea, setTarea] = useState("");
+  const [tareas, setTareas] = useState([]);
+
   return (
     <section>
       <Form>
@@ -10,14 +14,18 @@ const FormularioTareas = () => {
           className="mb-3 d-flex"
           controlId="exampleForm.ControlInput1"
         >
-         {/*  <Form.Label>Ingresa tus Tareas:</Form.Label> */}
+          {/*  <Form.Label>Ingresa tus Tareas:</Form.Label> */}
           <Form.Control
             type="text"
             placeholder="Ej: Tarea 1"
             minLength={3}
             maxLength={50}
+            onChange={(e) => setTarea(e.target.value)}
+            value={tarea}
           />
-          <Button variant="info" className="mx-2" type="submit">Agregar</Button>
+          <Button variant="info" className="mx-2" type="submit">
+            Agregar
+          </Button>
         </Form.Group>
       </Form>
       <ListaTareas></ListaTareas>
