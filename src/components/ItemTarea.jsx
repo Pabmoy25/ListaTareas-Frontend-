0d2a41188b9f27641c508783bff77243894a0da5
Pabmoy25 <pabmoy1725@gmail.com>
2024-01-30 21:00:13 -0300

    se crea el boton borrar y se realiza maquetado

--- src/components/ItemTarea.jsx
+++ src/components/ItemTarea.jsx
@@ -1,11 +1,12 @@
-import { ListGroup } from "react-bootstrap";
+import { Button, ListGroup } from "react-bootstrap";
 
-const ItemTarea = ({nombreTarea}) => {
-    return (
-        <div>
-             <ListGroup.Item>{nombreTarea}</ListGroup.Item>
-        </div>
-    );
+const ItemTarea = ({ nombreTarea }) => {
+  return (
+    <ListGroup.Item className="d-flex justify-content-between align-items-center">
+      {nombreTarea}
+      <Button variant="danger">X</Button>
+    </ListGroup.Item>
+  );
 };
 
 export default ItemTarea;
